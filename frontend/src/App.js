82c38755a12e2a00533618b645a72b65e60eedb4
amcmu <amcmullen9291@gmail.com>
2021-06-11 14:08:52 -0400

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <img src="src/images/banner.gif" alt="banner"></img>
+    <center><button class="myButton">Past Notes</button><button class="myButton">Something New</button></center>
+    </div>
+    </>
+  );
+}
+
+export default App;
