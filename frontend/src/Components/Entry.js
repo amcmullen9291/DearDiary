42c366b409ce93693a5847cfff4539e179c0d5d9
amcmu <amcmullen9291@gmail.com>
2021-06-12 00:43:43 -0400

    starting css for Entry.js

--- frontend/src/Components/Entry.js
+++ frontend/src/Components/Entry.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 class Home extends React.Component {
     render() {
       return (
           <>
+          <body>
         <div className="App">
-        <center><h1 className="titleBanner">ᗪEᗩᖇ ᗪIᗩᖇY..</h1></center>
-        
+        <center><h1 className="titleBanner"><Link to="/">ᗪEᗩᖇ ᗪIᗩᖇY..</Link></h1></center>
+        <center><form >
+            <input className="paper" name="content"></input>
+        </form></center>
         
         <center><button class="myButton">Past Notes</button><button class="myButton">Something New</button></center>
         </div>
+        </body>
         </>
       )
     }
