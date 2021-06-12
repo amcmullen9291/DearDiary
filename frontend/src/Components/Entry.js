62f5a21d970b24de85d9bda4418e2f470ee6e7c4
amcmu <amcmullen9291@gmail.com>
2021-06-12 06:13:35 -0400

    added date to entryPage, added css animation to btns. added react moments dep

--- frontend/src/Components/Entry.js
+++ frontend/src/Components/Entry.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Name from './Name.js';
+import moment from 'moment';
+
 class Home extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentDate: new Date(),
+      today: moment(new Date()).format("dddd")      
+  }
+}
+
     render() {
       return (
           <>
@@ -9,14 +19,20 @@ class Home extends React.Component {
         <div className="App">
         <center><h1 className="titleBanner"><Link to="/">ᗪEᗩᖇ ᗪIᗩᖇY..</Link></h1></center>
         <div className="paper">
-        <form className="paperss">
+        <form className="paperss" id="entryForm">
+            <input type="text"  className="entryHeader" name="title" placeholder="the reason for my visit:"></input><br/>
+            <br/>
             <lable>♥ I just wanted to tell you ♥</lable><br/>
             <br/>
             <textarea className="papers" name="content"></textarea><br/>
             <br/>
-            <input type="submit" name="submit" value="Okay"></input>
+              {this.state.today}
+            <br/>
+            <br/>
+            {/* <input className="entrySubmit" type="submit" name="submit" value="Okay"></input> */}
+            <button  className="entrySubmit" onclick="document.getElementById('entryForm').submit()">Okay</button>
         </form>
-        <Name/>
+        {/* <Name/> */}
         </div>
         <center><button class="myButton">Past Notes</button><button class="myButton"><Link to="/Home">B A C K H O M E</Link></button></center>
         </div>
