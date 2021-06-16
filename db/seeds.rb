c043587296de2409888b06dc4d0718cd1942b5e4
amcmullen9291 <amcmullen9291@gmail.com>
2021-06-16 01:32:33 -0400

    added additional seed data. uncommented cors.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Entry.create!(:title => "Hello...", :content => "Welcome to DearDiary")
+Entry.create!(:title => "Well, Hello", :content => "My name is Jacques cousteau I was born on July 21 1899. My favourite authors are Kipling, O. Henry and Steuart Edward White. My favourite flower is Lady Slipper and Tiger Lily. My favourite sports are Trout fishing, Hiking, shooting, football and boxing. My favourite studies are English, Zoology and Chemistry. I intend to travel and write.")
+Entry.create!(:title => "I got a new puppy dog", :content => "I got a new puppy I will name him Browser. He is cute.");
