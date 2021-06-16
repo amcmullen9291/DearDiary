76123f7aa3e5d5b0ba93bf1b9287ddd6825c431d
amcmullen9291 <amcmullen9291@gmail.com>
2021-06-16 10:08:38 -0400

    safety commit

--- frontend/src/Components/SelectedEntry.js
+++ frontend/src/Components/SelectedEntry.js
@@ -3,20 +3,21 @@ import { useSelector } from 'react-redux';
 
 
  const SelectedEntry = () => {
-   const notes = useSelector((state) => state.Index.entries.data);
-   
-   if (notes) {
-  const title = notes[0].attributes.title;
-  const title2 = notes[1].attributes.title;
-  const title3 = notes[2].attributes.title;
+  const notes = useSelector((state) => state.Index.entries.data);
 
-   return (
-     <>
-     <li>{title}</li>
-     <li>{title2}</li>
-     <li>{title3}</li>
-     </>
-   )
- }
-};
+   if (notes) {
+      const renderIndex = notes.map(entry => {
+        return <ul key={entry.id}>{entry.title}</ul>
+      })
+    }
+    return(
+      <>
+      {SelectedEntry}
+      </>
+    )
+  }
+
+  // const title = notes[0].attributes.title;
+  // const title2 = notes[1].attributes.title;
+  // const title3 = notes[2].attributes.title;
 export default SelectedEntry;
